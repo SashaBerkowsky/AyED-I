deb18e966e31f7b821273e343bba91fc4addece9
Sasha Berkowsky <s.nberkowsky@gmail.com>
2023-10-09 10:30:33 -0300

    primer parcial & readme update

---
+++ Parciales/1 - Haskell/parcial.hs
@@ -0,0 +1,50 @@
+-- 1
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use foldr" #-}
+golesDeNoGoleadores :: [(String, String)] -> [Int] -> Int -> Int
+golesDeNoGoleadores goleadoresPorEquipo goles totalesGolesTorneo = totalesGolesTorneo - sumatoriaGoles goles
+
+sumatoriaGoles :: [Int] -> Int
+sumatoriaGoles [] = 0
+sumatoriaGoles (x:xs) = x + sumatoriaGoles xs
+
+-- 2
+-- interpreto que es imposible que hayan 2 jugadores con el mismo nombre jugando en equipos distintos
+equiposValidos :: [(String, String)] -> Bool
+equiposValidos [] = True
+equiposValidos ((equipo, jugador): xs) | perteneceAEquipo equipo xs || perteneceAEquipo jugador xs || perteneceAJugador equipo xs || perteneceAJugador jugador xs || jugador == equipo = False
+                                       | otherwise = equiposValidos xs
+
+perteneceAJugador :: String -> [(String, String)] -> Bool
+perteneceAJugador _ [] = False
+perteneceAJugador e ((equipo, jugador):xs) | e == jugador = True
+                                           | otherwise = perteneceAJugador e xs
+
+perteneceAEquipo :: String -> [(String, String)] -> Bool
+perteneceAEquipo _ [] = False
+perteneceAEquipo e ((equipo, jugador):xs) | e == equipo = True
+                                          | otherwise = perteneceAEquipo e xs
+
+-- 3
+porcentajeDeGoles :: String -> [(String, String)] -> [Int] -> Float
+porcentajeDeGoles jugador ((equipo, goleador):gs) (cantGoles:cs) = division golesDeJugador golesDeGoleadores * 100
+                                                                where golesDeGoleadores = sumatoriaGoles (cantGoles: cs)
+                                                                      golesDeJugador = obtenerGolesDeJugador jugador ((equipo, goleador): gs) (cantGoles: cs)
+
+division :: Int -> Int -> Float
+division a b = (fromIntegral a) / (fromIntegral b) 
+
+obtenerGolesDeJugador :: String -> [(String, String)] -> [Int] -> Int
+obtenerGolesDeJugador jugador ((equipo, goleador):gs) (cantGoles: cs) | goleador == jugador = cantGoles
+                                                                      | otherwise = obtenerGolesDeJugador jugador gs cs
+
+-- 4
+botinDeOro :: [(String, String)] -> [Int] -> String
+botinDeOro ((equipo, goleador):gs) (cantGoles:cs) | esMaximo cantGoles cs = goleador
+                                                  | otherwise = botinDeOro gs cs
+
+esMaximo :: Int -> [Int] -> Bool
+esMaximo _ [] = True
+esMaximo e (x:xs) | e < x = False
+                  | otherwise = esMaximo e xs
--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # AyED-I
+
 Repositorio de Algoritmos y Estructuras de Datos I (me gusta mas como suena)
+
 > Muy posiblemente hayan errores y algunos ejercicios que quedaron en el olvido
 
 ## [Prácticas](https://github.com/SashaBerkowsky/AyED-I/tree/main/Pr%C3%A1cticas)
-|Práctica | Enunciados | Soluciones |
-| -- | -- | -- |
-|[3](Prácticas/Práctica%203) | [Introducción a Haskell](Prácticas/Práctica%203/enunciado.pdf) | [Práctica 3](Prácticas/Práctica%203/practica3.hs) |
-|[4](Prácticas/Práctica%4) | [Recursión sobre enteros](Prácticas/Práctica%204/enunciado.pdf) | [Práctica 4](Prácticas/Práctica%204/practica4.hs) |
-|[5](Prácticas/Práctica%205) | [Recursión sobre Listas](Prácticas/Práctica%205/enunciado.pdf) | [Práctica 5](Prácticas/Práctica%205/practica5.hs) |
-|[6](Prácticas/Práctica%206) | [Recursión sobre Listas](Prácticas/Práctica%206/enunciado.pdf) | [Práctica 6](Prácticas/Práctica%206/practica6.py) |
+
+| Práctica                    | Enunciado                                                       | Solución                                          |
+| --------------------------- | --------------------------------------------------------------- | ------------------------------------------------- |
+| [3](Prácticas/Práctica%203) | [Introducción a Haskell](Prácticas/Práctica%203/enunciado.pdf)  | [Práctica 3](Prácticas/Práctica%203/practica3.hs) |
+| [4](Prácticas/Práctica%4)   | [Recursión sobre enteros](Prácticas/Práctica%204/enunciado.pdf) | [Práctica 4](Prácticas/Práctica%204/practica4.hs) |
+| [5](Prácticas/Práctica%205) | [Recursión sobre Listas](Prácticas/Práctica%205/enunciado.pdf)  | [Práctica 5](Prácticas/Práctica%205/practica5.hs) |
+| [6](Prácticas/Práctica%206) | [Recursión sobre Listas](Prácticas/Práctica%206/enunciado.pdf)  | [Práctica 6](Prácticas/Práctica%206/practica6.py) |
+
+## [Parciales](/Parciales)
+
+| Parcial                         | Enunciado                                          | Solución                                          |
+| ------------------------------- | -------------------------------------------------- | ------------------------------------------------- |
+| [1](/Parciales/1%20-%20Haskell) | [Tema 1](/Parciales/1%20-%20Haskell/enunciado.pdf) | [Solución](/Parciales/1%20-%20Haskell/parcial.hs) |
 
 ## [Simulacros de examen](https://github.com/SashaBerkowsky/AyED-I/tree/main/Simulacros%20de%20examen/Simulacro%20primer%20parcial)
+
 - [Simulacro Primer Parcial I](https://github.com/SashaBerkowsky/AyED-I/tree/main/Simulacros%20de%20examen/Simulacro%20primer%20parcial)
