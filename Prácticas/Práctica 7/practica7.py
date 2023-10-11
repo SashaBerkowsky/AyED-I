b5c561fe772681e0d7437879b551f7cf5b921342
Sasha Berkowsky <s.nberkowsky@gmail.com>
2023-10-11 20:22:26 -0300

    practica 7 ej 1.5

--- Prácticas/Práctica 7/practica7.py
+++ Prácticas/Práctica 7/practica7.py
@@ -10,17 +10,37 @@ def pertenece_C(s, e) -> bool:
 
 # 1.2
 def divide_a_todos(s: list[int], e:int) -> bool:
-    i = 0
-    longitud_secuencia = len(s)
+    i: int = 0
+    longitud_secuencia: int = len(s)
     while (i < longitud_secuencia and s[i] % e == 0):
         i+=1
     return i == longitud_secuencia
 
 # 1.3
 def suma_total(s: list[int]) -> int:
-    suma = 0
+    suma: int = 0
     for e in s:
         suma += e
     return suma
 
-print(suma_total([1,2,3,4,5,6]))
+# 1.4
+def ordenados(s: list[int]) -> bool:
+    i: int = 0
+    longitud_secuencia: int = len(s)
+    while(i < longitud_secuencia - 1 and s[i] < s[i+1]):
+        i+=1
+    return i == longitud_secuencia - 1
+
+# 1.5
+def longitud_mayor_7(frase: str) -> bool:
+    i: int = 0
+    palabras: list[str] = frase.split(' ')
+    cantidad_palabras: int = len(palabras)
+    while(i < cantidad_palabras and len(palabras[i]) <= 7):
+        i += 1
+    return i != cantidad_palabras
+
+
+    
+        
+
